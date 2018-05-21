8165e6b4175f08e34ceb5bd623176e2546cb2e62
hys <huyansheng@fishtrip.cn>
2018-05-21 20:14:44 +0800

    feat: git init fork from @rematch/immer for bug

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Models, Plugin } from '@rematch/core'
+import produce from 'immer'
+import { combineReducers, ReducersMapObject } from 'redux'
+
+function combineReducersWithImmer(reducers: ReducersMapObject) {
+  const reducersWithImmer = {}
+
+  // reducer must return value because literal don't support immer
+  for (const [key, reducerFn] of Object.entries(reducers)) {
+    reducersWithImmer[key] = (state, payload) => {
+      if (typeof state === 'object') {
+        return produce(state, (draft: Models) => {
+          reducerFn(draft, payload)
+        })
+      } else {
+        return reducerFn(state, payload)
+      }
+    }
+  }
+
+  return combineReducers(reducersWithImmer)
+}
+
+// rematch plugin
+const immerPlugin = (): Plugin => ({
+  config: {
+    redux: {
+      combineReducers: combineReducersWithImmer,
+    },
+  },
+})
+
+export default immerPlugin
